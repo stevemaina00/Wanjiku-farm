656e80c047711a7d0451b5fe767e3ec5beb0223a
Steve <steve.maina@student.moringaschool.com>
2022-09-01 20:00:43 +0300

    add containers

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,25 @@ document.querySelector('#logo').addEventListener('mouseover', (e)=>{
     
 })
 document.querySelector('#logo').addEventListener('mouseover', (e)=>{
+    e.preventDefault()
     alert('Welcome to our website!')
 }, 2000)
-
+document.getElementById("bt").addEventListener('click', (e)=>{
+    e.preventDefault();
+    let A=document.getElementById("shedA").value
+    let B=document.getElementById("shedB").value
+    let total=parseInt(A) + parseInt(B);
+    document.getElementById("day").innerHTML=`Your total litres for today is ${total} L`
+    let sp = 50;
+    let totalPrice=total * sp;
+    document.getElementById("totalPrice").innerHTML=`Congratulations you have earned 
+    ${totalPrice}kes.Check your gmail for more information`})
+    document.getElementById("bt2").addEventListener('click', (e)=>{
+    let x=document.getElementById("nestA").value
+    let y=document.getElementById("nestB").value
+    let eggtotal=parseInt(x) + parseInt(y);
+    document.getElementById("eggday").innerHTML=`Your total trays for today is ${eggtotal} `
+    let sp2 = 400;
+    let totalPrice2=eggtotal * sp2;
+    document.getElementById("eggtotalPrice").innerHTML=`Congratulations you have earned 
+    ${totalPrice2}kes.Check your gmail for more information`});
